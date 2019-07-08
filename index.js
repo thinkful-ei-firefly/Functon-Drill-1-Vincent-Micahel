b698cb2810a1d514cdb98df89f632eecddce6cfb
m-cal <iengineersoftware@gmail.com>
2019-07-08 16:11:14 -0400

    finished question 7 part 1

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 'use strict';
- 
+
+
+
 function getYearOfBirth(age) {
-    return 2019 - age;
+  if (age < 0) {
+    throw new Error('Age can not be negative');
+  }
+  return 2019 - age;
 }
+
 function createGreeting(name, age) {
-    const yob = getYearOfBirth(age);
-    return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${getYearOfBirth(age)}`;
+  const yob = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${getYearOfBirth(age)}`;
 }
-console.log(createGreeting("Vincent", 23));
+
+
+try {
+  const greeting1 = createGreeting('Mike', 22);
+  console.log(greeting1);
+} catch(e) {
+  console.error(`${e.message}`);
+}
+
