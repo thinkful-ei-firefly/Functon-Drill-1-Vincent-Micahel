527cefd8609d315a8c866bfd43e0de1de12ad282
vbc221 <vbc221@nyu.edu>
2019-07-08 14:25:42 -0500

    change

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
-function createGreeting(name, age){
-    const yearOfBirth= 2019-age;
-    return createGreeting
+function createGreeting(name,age){ 
+    const yearOfBirth = 2019 - age;
+    return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}` ;  
 }
+
+const greeting1= createGreeting('Vincent',23);
+console.log(greeting1);
 
